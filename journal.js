4fccd7ffc4dff4e2d2e6f12e4cf796f5e8675f93
Amin Shahi <your@email.com>
2026-01-18 01:21:23 -0800

    Add add and milesToKm functions

--- journal.js
+++ journal.js
@@ -7,3 +7,12 @@ function greet(name) {
 function isEven(num) {
     return num % 2 === 0;
 }
+// Add two numbers
+function add(a, b) {
+    return a + b;
+}
+
+// Convert miles to kilometers
+function milesToKm(miles) {
+    return miles * 1.60934;
+}
